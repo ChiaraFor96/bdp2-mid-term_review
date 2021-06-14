50ea866bfefa6786d97bc33789a288e79894feca
Chiara Forresi <chiara@forresi.it>
2021-06-14 14:04:29 +0000

    add persistency of notebooks

--- docker/startcontainer.sh
+++ docker/startcontainer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker pull chiaraforresi/bdp2-mid-term_review
-docker run -p 8888:8888 chiaraforresi/bdp2-mid-term_review
+docker run -v $PWD/work:/home/jovyan/work -p 8888:8888 chiaraforresi/bdp2-mid-term_review
